d9f17cdaf9dea37e449b9af4225e820ceda9370f
Dan Dumitriu <dandumitriu33@gmail.com>
2021-03-28 13:00:46 +0300

    Add initial Web project

---
+++ ui-grid-test/ui-grid-test.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ui_grid_test.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
