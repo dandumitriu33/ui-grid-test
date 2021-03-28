f143a9beb9abf12b1f61bc3ca3bf8ea77121c2df
Dan Dumitriu <dandumitriu33@gmail.com>
2021-03-28 13:37:59 +0300

    Add simple grid via Controller and $scope

---
+++ ui-grid-test/ui-grid-test.Web/wwwroot/Scripts/App/controllers/MainController.js
@@ -0,0 +1,23 @@
+﻿app.controller('MainController', function ($scope) { 
+    $scope.message = "Hi from Main Controller scope";
+    $scope.myData = [
+        {
+            firstName: "Cox",
+            lastName: "Carney",
+            company: "Enormo",
+            employed: true
+        },
+        {
+            firstName: "Lorraine",
+            lastName: "Wise",
+            company: "Comveyer",
+            employed: false
+        },
+        {
+            firstName: "Nancy",
+            lastName: "Waters",
+            company: "Fuelton",
+            employed: false
+        }
+    ];
+});
